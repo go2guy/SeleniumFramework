59a2f51ebcb7eb595d4706de48f78f91e060f024
herbert scruggs <herbscruggs@gmail.com>
2017-04-19 23:01:11 -0500

    added in some write functionality tests for excel

--- src/main/java/com/utils/ExcelUtils.java
+++ src/main/java/com/utils/ExcelUtils.java
@@ -1,49 +1,122 @@
 package com.utils;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.List;
 
 import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ExcelUtils {
 
-	private static XSSFSheet ExcelWSheet;
-	private static XSSFWorkbook ExcelWBook;
-	private static XSSFCell Cell;
+    private static XSSFWorkbook ExcelWBook;
+    private static XSSFSheet ExcelWSheet;
+    private static XSSFCell Cell;
 
-	public static void setExcelFile(String Path, String SheetName) throws Exception {
-		try {
-			// Open the Excel file
-			FileInputStream ExcelFile = new FileInputStream(Path);
-			// Access the required test data sheet
-			ExcelWBook = new XSSFWorkbook(ExcelFile);
-			ExcelWSheet = ExcelWBook.getSheet(SheetName);
-		} catch (Exception e) {
-			throw (e);
-		}
-	}
+    public static void setExcelFile(String Path, String SheetName) throws Exception {
+        try {
+            // Open the Excel file
+            FileInputStream ExcelFile = new FileInputStream(Path);
+            // Access the required test data sheet
+            ExcelWBook = new XSSFWorkbook(ExcelFile);
+            ExcelWSheet = ExcelWBook.getSheet(SheetName);
+        } catch (Exception e) {
+            throw (e);
+        }
+    }
 
-	// This method is to read the test data from the Excel cell, in this we are
-	// passing parameters as Row num and Col num
+    // This method is to read the test data from the Excel cell, in this we are
+    // passing parameters as Row num and Col num
 
-	public static String getCellData(int RowNum, int ColNum) throws Exception {
-		try {
+    public static String getCellData(int RowNum, int ColNum) throws Exception {
+        try {
 
-			Cell = ExcelWSheet.getRow(RowNum).getCell(ColNum);
+            Cell = ExcelWSheet.getRow(RowNum).getCell(ColNum);
 
-			switch (Cell.getCellType()) {
-			case 0:
-				return String.valueOf(Cell.getNumericCellValue());
-			case 1:
-				return Cell.getStringCellValue();
-			default:
-				return "";
-			}
+            switch (Cell.getCellType()) {
+                case 0:
+                    return String.valueOf(Cell.getNumericCellValue());
+                case 1:
+                    return Cell.getStringCellValue();
+                default:
+                    return "";
+            }
 
-		} catch (Exception e) {
-			return "";
-		}
-	}
+        } catch (Exception e) {
+            return "";
+        }
+    }
+
+    public static String createWorksheet(String Path, String SheetName) throws Exception {
+        File file = new File(Path);
+        if (!file.exists())
+            ExcelWBook = new XSSFWorkbook();
+
+        try {
+
+            ExcelWSheet = ExcelWBook.createSheet(SheetName);
+
+            writeOut(Path);
+            return SheetName;
+        } catch (Exception e) {
+            return "";
+
+        }
+    }
+
+
+    public static void writeRow(String Path, Object[][] data) throws IOException {
+
+        int rowCount = ExcelWSheet.getLastRowNum();
+
+        for (Object[] payload : data) {
+
+            XSSFRow row = ExcelWSheet.createRow(rowCount++);
+
+            int columnCount = 0;
+
+            for (Object field : payload) {
+                Cell = row.createCell(columnCount++);
+                if (field instanceof String) {
+                    Cell.setCellValue((String) field);
+                } else if (field instanceof Integer) {
+                    Cell.setCellValue((Integer) field);
+                }
+            }
+
+        }
+
+        writeOut(Path);
+    }
+
+    public static void writeRow(String Path, List<String> data) throws IOException {
+
+        int rowCount = ExcelWSheet.getPhysicalNumberOfRows();
+
+        XSSFRow row = ExcelWSheet.createRow(rowCount++);
+
+        int columnCount = 0;
+
+        for (String field : data) {
+            Cell = row.createCell(columnCount++);
+            Cell.setCellValue((String) field);
+        }
+
+        writeOut(Path);
+    }
+
+    private static void writeOut(String Path) throws FileNotFoundException, IOException {
+        FileOutputStream fileOut = new FileOutputStream(Path);
+
+        //write this workbook to an Outputstream.
+        ExcelWBook.write(fileOut);
+        fileOut.flush();
+        fileOut.close();
+    }
 
 }
--- src/test/java/com/htmltables/HtmlTable.java
+++ src/test/java/com/htmltables/HtmlTable.java
@@ -1,5 +1,8 @@
 package com.htmltables;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.testng.Assert;
 import org.testng.ITestContext;
 import org.testng.ITestResult;
@@ -15,47 +18,88 @@ import com.utils.TestngContext;
 
 public class HtmlTable extends BaseUtils {
 
-	private TablePages tp;
-	private static String EXCEL_FILE = "src/test/resources/HTMLTables.xlsx";
-	private static String EXCEL_WORKSHEET1 = "HTML Table";
-	private static String EXCEL_WORKSHEET2 = "HTML Table 2";
+    private TablePages tp;
+    private static String EXCEL_FILE = "src/test/resources/HTMLTables.xlsx";
+    private static String EXCEL_FILE2 = "src/test/resources/test.xlsx";
+    private static String EXCEL_WORKSHEET1 = "HTML Table";
+    private static String EXCEL_WORKSHEET2 = "HTML Table 2";
 
-	@BeforeClass(alwaysRun = true)
-	public void launchBrowser(ITestContext context) {
-		TestngContext.setContext(context); // sets the context from Testng,xml
-		setDriver(); // sets the driver
-		NavigatToUrl(); // Navigate to primary webpage
+    @BeforeClass(alwaysRun = true)
+    public void launchBrowser(ITestContext context) {
+        TestngContext.setContext(context); // sets the context from Testng,xml
+        setDriver(); // sets the driver
+        NavigatToUrl(); // Navigate to primary webpage
 
-		tp = new TablePages(getDriver());
-	}
+        tp = new TablePages(getDriver());
+    }
 
-	@Test(priority = 10)
-	public void verifyWorksheetHTMLTable() throws Exception {
+    @Test(priority = 10)
+    public void verifyWorksheetHTMLTable() throws Exception {
 
-		tp.printTable();
-		ExcelUtils.setExcelFile(EXCEL_FILE, EXCEL_WORKSHEET1);
-		Assert.assertEquals(tp.getTableHeader(tp.tableRows, 0, 0), ExcelUtils.getCellData(0, 0));
-		Assert.assertEquals(tp.getTableCell(tp.tableRows, 1, 0), ExcelUtils.getCellData(1, 0));
+        tp.printTable();
+        ExcelUtils.setExcelFile(EXCEL_FILE, EXCEL_WORKSHEET1);
+        Assert.assertEquals(tp.getTableHeader(tp.tableRows, 0, 0), ExcelUtils.getCellData(0, 0));
+        Assert.assertEquals(tp.getTableCell(tp.tableRows, 1, 0), ExcelUtils.getCellData(1, 0));
 
-	}
+    }
 
-	@Test(priority = 20)
-	public void verifyWorksheet2HTMLTable() throws Exception {
+    @Test(priority = 20)
+    public void verifyWorksheet2HTMLTable() throws Exception {
 
-		ExcelUtils.setExcelFile(EXCEL_FILE, EXCEL_WORKSHEET2);
-		Assert.assertEquals("Country", ExcelUtils.getCellData(0, 0));
-		Assert.assertEquals("Germany", ExcelUtils.getCellData(1, 0));
+        ExcelUtils.setExcelFile(EXCEL_FILE, EXCEL_WORKSHEET2);
+        Assert.assertEquals("Country", ExcelUtils.getCellData(0, 0));
+        Assert.assertEquals("Germany", ExcelUtils.getCellData(1, 0));
 
-	}
+    }
 
-	@AfterMethod(alwaysRun = true)
-	public void validateTest(ITestResult result) {
-		takeScreenShot(result);
-	}
 
-	@AfterClass(alwaysRun = true)
-	public void terminateApp() {
-		tearDown();
-	}
+    @Test(priority = 30)
+    public void writeFileAlist() throws Exception {
+
+        ExcelUtils.createWorksheet(EXCEL_FILE2, EXCEL_WORKSHEET1);
+
+        List<String> header = Arrays.asList("Country", "Contact", "Company");
+        ExcelUtils.writeRow(EXCEL_FILE2, header);
+        List<String> data = Arrays.asList("Germany", "Maria Anders", "Alfreds Futterkiste");
+        ExcelUtils.writeRow(EXCEL_FILE2, data);
+        data = Arrays.asList("Mexico", "Francisco Chang", "Centro comercial Moctezuma");
+        ExcelUtils.writeRow(EXCEL_FILE2, data);
+        data = Arrays.asList("Austria", "Roland Mendel", "Ernst Handel");
+        ExcelUtils.writeRow(EXCEL_FILE2, data);
+        data = Arrays.asList("UK", "Helen Bennett", "Island Trading");
+        ExcelUtils.writeRow(EXCEL_FILE2, data);
+        data = Arrays.asList("Canada", "Yoshi Tannamuri", "Laughing Bacchus Winecellars");
+        ExcelUtils.writeRow(EXCEL_FILE2, data);
+        data = Arrays.asList("Italy", "Giovanni Rovelli", "Magazzini Alimentari Riuniti");
+        ExcelUtils.writeRow(EXCEL_FILE2, data);
+
+    }
+
+    @Test(priority = 40)
+    public void writeFileAsObject() throws Exception {
+
+        ExcelUtils.createWorksheet(EXCEL_FILE2, EXCEL_WORKSHEET2);
+
+        Object[][] data = {
+            {"Head First Java", "Kathy Serria", 79},
+            {"Effective Java", "Joshua Bloch", 36},
+            {"Clean Code", "Robert martin", 42},
+            {"Thinking in Java", "Bruce Eckel", 35},
+        };
+
+
+        ExcelUtils.writeRow(EXCEL_FILE2, data);
+
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void validateTest(ITestResult result) {
+        takeScreenShot(result);
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void terminateApp() {
+        tearDown();
+    }
 
 }
